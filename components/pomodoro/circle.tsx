f747adbc47278f49b74f996a9e7bf4b6447b5f9b
changmin <developerQuo@gmail.com>
2024-06-07 19:24:43 +0900

    feat: add terminating the circle

--- components/pomodoro/circle.tsx
+++ components/pomodoro/circle.tsx
@@ -1,7 +1,21 @@
 type InputProps = {
 	handlePress: () => void;
+	handleLongPress: () => void;
+	handleReleasePress: () => void; // 누르다가 손 떼는 경우. timer 제거.
 };
 
-export default function DrainingCircle({ handlePress }: InputProps) {
-	return <div className="w-40 h-40 rounded-full" onClick={handlePress}></div>;
+export default function DrainingCircle({
+	handlePress,
+	handleLongPress,
+	handleReleasePress,
+}: InputProps) {
+	return (
+		<div
+			data-testid="draining-circle"
+			className="w-40 h-40 rounded-full"
+			onClick={handlePress}
+			onMouseDown={handleLongPress}
+			onMouseUp={handleReleasePress}
+		></div>
+	);
 }
