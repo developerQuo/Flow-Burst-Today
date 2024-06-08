c5c7fe86fc89a56e92bd689a0a32afd77a2c31df
changmin <developerQuo@gmail.com>
2024-06-08 09:07:01 +0900

    feat: update storybook setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import DrainingCircle from "@/components/pomodoro/circle";
+import { Pomodoro } from "@/utils/timer";
+import { useState } from "react";
+
 export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			Pomodoro
-		</main>
-	);
+  const [pomodoro] = useState(new Pomodoro());
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Pomodoro
+      <DrainingCircle pomodoro={pomodoro} />
+    </main>
+  );
 }
--- components/pomodoro/circle.tsx
+++ components/pomodoro/circle.tsx
@@ -1,36 +1,39 @@
-import { Pomodoro } from '@/utils/timer';
-import { useRef } from 'react';
+import { Pomodoro } from "@/utils/timer";
+import { useRef } from "react";
 
-type InputProps = {
-	pomodoro: Pomodoro;
+export type InputProps = {
+  pomodoro: Pomodoro;
 };
 
 export default function DrainingCircle({ pomodoro }: InputProps) {
-	const longPressTimer = useRef<NodeJS.Timeout | undefined>(undefined);
+  const longPressTimer = useRef<NodeJS.Timeout | undefined>(undefined);
 
-	const handlePress = () => {
-		pomodoro.onTimer();
-	};
+  const handlePress = () => {
+    alert("clicked");
+    pomodoro.onTimer();
+  };
 
-	const handleLongPress = () => {
-		longPressTimer.current = setTimeout(() => {
-			pomodoro.offTimer();
-		}, 2000);
-	};
+  const handleLongPress = () => {
+    longPressTimer.current = setTimeout(() => {
+      pomodoro.offTimer();
+    }, 2000);
+  };
 
-	const handleReleasePress = () => {
-		if (longPressTimer?.current) {
-			clearTimeout(longPressTimer.current);
-		}
-	};
+  const handleReleasePress = () => {
+    if (longPressTimer?.current) {
+      clearTimeout(longPressTimer.current);
+    }
+  };
 
-	return (
-		<div
-			data-testid="draining-circle"
-			className="w-40 h-40 rounded-full"
-			onClick={handlePress}
-			onMouseDown={handleLongPress}
-			onMouseUp={handleReleasePress}
-		></div>
-	);
+  return (
+    <>
+      <div
+        data-testid="draining-circle"
+        className="h-60 w-60 rounded-full border bg-gray-400"
+        onClick={handlePress}
+        onMouseDown={handleLongPress}
+        onMouseUp={handleReleasePress}
+      ></div>
+    </>
+  );
 }
