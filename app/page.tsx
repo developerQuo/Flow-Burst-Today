e5e1b4afd06e89f9b616276cbc090025125c0902
changmin <developerQuo@gmail.com>
2024-06-12 12:58:57 +0900

    refactor: change pomodoro instance state

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@
 
 import Hourglass from "@/components/pomodoro/hourglass";
 import { Pomodoro } from "@/lib/pomodoro";
-import { useState } from "react";
+import { useMemo } from "react";
 
+// TODO: pomodoro 어떻게 넘길지 체크
 export default function Home() {
-    const [pomodoro] = useState(new Pomodoro());
+    const pomodoro = useMemo(() => new Pomodoro(), []);
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             Pomodoro
