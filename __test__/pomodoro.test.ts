1003bfa682604144822b85b72f3952ed9bf6c548
changmin <developerQuo@gmail.com>
2024-07-04 17:42:42 +0900

    test: change lock-screen test to test rendered app

--- __test__/pomodoro.test.ts
+++ __test__/pomodoro.test.ts
@@ -2,13 +2,30 @@ import { describe, expect, it, test } from "@jest/globals";
 import { Pomodoro } from "@/lib/pomodoro";
 import { MINUTE, SECOND } from "@/utils/times";
 
+jest.useFakeTimers();
+
 describe("pomodoro timer", () => {
     let pomodoro: Pomodoro;
 
-    beforeEach(() => {
-        jest.useFakeTimers();
-        jest.spyOn(global, "setTimeout");
+    beforeAll(() => {
+        // Mock HTMLMediaElement.prototype.play
+        Object.defineProperty(HTMLMediaElement.prototype, "play", {
+            configurable: true,
+            value: jest.fn().mockResolvedValue(undefined),
+        });
 
+        // Mock navigator.wakeLock
+        Object.defineProperty(navigator, "wakeLock", {
+            writable: true,
+            value: {
+                request: jest.fn().mockResolvedValue({
+                    release: jest.fn(),
+                }),
+            },
+        });
+    });
+
+    beforeEach(() => {
         pomodoro = new Pomodoro();
     });
 
